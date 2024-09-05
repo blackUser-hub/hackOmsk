ab22d173ad3512d9b8f470c4122c107c6be330ce
blackUser-hub <glebforcom@gmail.com>
2024-09-05 21:13:36 +0300

    backend new

---
+++ backend/api/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+import db
+
+class User(db.Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    tg_id = Column(Integer)
+    username = Column(String)
+    fullname = Column(String)
+    status = Column(Integer)
+    tasks = Column(Integer)
+
