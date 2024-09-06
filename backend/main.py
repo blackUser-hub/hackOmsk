d6e78dd97ccb72e257daa1994fa0a5b09d2dfb93
blackUser-hub <glebforcom@gmail.com>
2024-09-06 18:39:40 +0300

    ghmcdthjkchjfgdtyjcfgydi

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,8 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import BaseModel
 from sqlalchemy.exc import IntegrityError
-import api.db
-import api.service
+import api.db as db
+import api.service as service
 
 
 app = FastAPI()
