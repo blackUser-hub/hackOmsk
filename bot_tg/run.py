08b75a5bbc873f035e421394e04d14cf0a189e72
d3putation <94278254+d3putation@users.noreply.github.com>
2024-09-06 19:56:51 +0500

    added bot

--- bot_tg/run.py
+++ bot_tg/run.py
@@ -1,1 +1,31 @@
-print("hello from bot")
+import logging, asyncio
+from aiogram import Bot, Dispatcher
+
+from config import TOKEN
+
+from app.handlers import router
+
+
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+async def main():
+    dp.include_routers(router)
+    
+
+    polling = asyncio.create_task(dp.start_polling(bot))
+    
+    await asyncio.gather(polling)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.warning('Bot stopped')  
